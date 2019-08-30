30529e088a726ddcc44b8413b15807497af42793
Liam P. White <byteslice@airmail.cc>
2019-08-29 21:01:25 -0400

    format

--- lib/philomena/images/policy.ex
+++ lib/philomena/images/policy.ex
@@ -10,10 +10,14 @@ defmodule Philomena.Images.Policy do
   end
 
   def authorize(:update, user, image) do
-    (image.hidden_from_users == false and image.user_id == user.id) or image_moderator?(user) or dupe_moderator?(user)
+    (image.hidden_from_users == false and image.user_id == user.id) or
+      image_moderator?(user) or
+      dupe_moderator?(user)
   end
 
-  def authorize(:update_metadata, user, image), do: authorize(:read, user, image) and image.tag_editing_allowed
+  def authorize(:update_metadata, user, image),
+    do: authorize(:read, user, image) and image.tag_editing_allowed
+
   def authorize(:hide, user, _image), do: image_moderator?(user)
   def authorize(:undelete, user, _image), do: image_moderator?(user)
   def authorize(:repair, user, _image), do: image_moderator?(user)
--- lib/philomena/policy.ex
+++ lib/philomena/policy.ex
@@ -1,5 +1,9 @@
 defmodule Philomena.Policy do
   def role?(user, name, resource_type) do
-    !!(user.roles |> Enum.find(fn %{name: ^name, resource_type: ^resource_type} -> true; _ -> false end))
+    !!(user.roles
+       |> Enum.find(fn
+         %{name: ^name, resource_type: ^resource_type} -> true
+         _ -> false
+       end))
   end
 end
